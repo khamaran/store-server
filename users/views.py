b6af23648caf1d1c57916a893497972d951bf9f0
Мария Тумилович <khamaran@yahoo.com>
2023-01-26 17:44:31 +0300

    5

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileFrom
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -33,6 +35,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileFrom(instance=request.user, data=request.POST, files=request.FILES)
@@ -44,7 +47,12 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileFrom(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    context = {
+        'title': 'Store - Профиль', 
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+        }
     return render(request, 'users/profile.html', context)
 
 
